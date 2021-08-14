e3d290b188b320824f1a129fd36f5add5c962876
TacoTalon <88513496+TacoTalon@users.noreply.github.com>
2021-08-14 11:03:43 -0400

    Play/Pause Rework
    Reworked play pause system with animations to replace the separate canvas screens. PlayPauseManager script will manage the states and references to its bool "toggleBool" can give a decent idea of whether the game is paused or not.

---
+++ Ludum 47/Assets/Scripts/MonoBehaviour/UI/pauseTextManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseTextManager : MonoBehaviour
+{
+    Animator ani;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ani = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Methods called by the playPauseManager to manage the two main animation states.
+    // The "substates" / inbetween states are found in the animation tab within unity.
+    public void slideIn() {
+        ani.Play("SlideVisible");
+    }
+
+    public void slideOut() {
+        ani.Play("SlideHide");
+    }
+
+}
---
+++ Ludum 47/Assets/Scripts/MonoBehaviour/UI/playPauseManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playPauseManager : MonoBehaviour
+{
+    Animator ani;
+    public bool toggleBool;
+    public Canvas mainCanvas;
+    public GameObject gamePanel;
+    public GameObject pausePanel;
+    public AnimationClip clip1;
+    public AnimationClip clip2;
+    private UI userI;
+    public pauseTextManager textManager;
+
+    // Start is called before the first frame update
+    void Start() {
+        ani = this.GetComponent<Animator>();
+        userI = mainCanvas.GetComponent<UI>();
+    }
+
+    void Update() {
+    }
+
+    public void toggle() {
+        // Manages states based on bool and flips it when called
+        if (toggleBool) {
+            pause();
+            textManager.slideIn();
+        } else {
+            resume();
+            textManager.slideOut();
+        }
+
+    }
+
+    
+    // Pause and resume methods replace the functions of the onClick() functions added directly to the legacy buttons.
+    // Adding code in the respective sections should be straightforward and hopefully doesnt break stuff
+    public void pause() {
+        // Pause code
+        toggleBool = false;
+        ani.Play(clip1.name);
+        userI.Pause();
+        gamePanel.SetActive(false);
+        pausePanel.SetActive(true);
+    }
+
+    public void resume() {
+        // Resume code
+        toggleBool = true;
+        ani.Play(clip2.name);
+        userI.Resume();
+        gamePanel.SetActive(true);
+        pausePanel.SetActive(false);
+    }
+}
