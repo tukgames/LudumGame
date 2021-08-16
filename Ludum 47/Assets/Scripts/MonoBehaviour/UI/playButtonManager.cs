e81fc956f87b3725592c1f41fa3bb94f92a8bbad
TacoTalon <88513496+TacoTalon@users.noreply.github.com>
2021-08-16 15:39:56 -0400

    Play and dead screen overhaul
    overhaul of play and dead screen. added animations to the play segment, zooming in and added the framework for score, coins, and shop display.

---
+++ Ludum 47/Assets/Scripts/MonoBehaviour/UI/playButtonManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playButtonManager : MonoBehaviour
+{
+    
+    public bool doStartScreen = true;
+    public UI ui;
+    public GameObject playPause;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (doStartScreen) {
+            playPause.SetActive(false);
+            ui.Pause();
+        } else {
+            this.gameObject.SetActive(false);
+        }
+
+        Debug.Log("dripswag");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startSequence() {
+        Debug.Log("Start sequence started");
+        ui.Resume();
+        playPause.SetActive(true);
+        
+    }
+
+    public void destroyButton() {
+        this.gameObject.SetActive(false);
+    }
+
+}
---
+++ Ludum 47/Assets/Scripts/MonoBehaviour/UI/restartFunction.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class restartFunction : MonoBehaviour
+{
+    public UI ui;
+    public GameObject deadPanel;
+
+    // This script exists only so unity's stupid (and useful) event system works
+    
+    public void runRestart() {
+        ui.Restart();
+        deadPanel.gameObject.SetActive(false);
+
+    }
+}
