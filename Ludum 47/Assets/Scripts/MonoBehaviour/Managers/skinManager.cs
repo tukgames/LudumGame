ab568e07eb6418a9b177078f94023100b3817129
TacoTalon <88513496+TacoTalon@users.noreply.github.com>
2022-02-27 12:40:53 -0500

    Skin system
    Adds skinManager class to the player prefab. The skin sprite can be changed there. Should accept and scale any image that is circular.

---
+++ Ludum 47/Assets/Scripts/MonoBehaviour/Managers/skinManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skinManager : MonoBehaviour
+{
+
+    private GameObject skinHolder;
+    private SpriteRenderer shRenderer;
+    private SpriteRenderer pRenderer;
+    private Sprite parentSprite;
+    public Sprite skin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Parent's Renderer
+        pRenderer = this.GetComponent<SpriteRenderer>();
+
+        // Initialize Skin Holder Object
+        skinHolder = new GameObject();
+        skinHolder.name = "skinHolder";
+
+        // Modify Skin Holder
+        skinHolder.transform.parent = this.transform;
+        shRenderer = skinHolder.AddComponent<SpriteRenderer>();
+        shRenderer.sprite = skin;
+        shRenderer.sortingOrder = pRenderer.sortingOrder + 1;
+
+
+        // Adjust scale by dividing bound size of parent by the skin then multiplying by the inverse scale of the parent
+        // I do not know why this works but I do not want to do the math to learn why it does
+        float adjustedScale = (pRenderer.bounds.size.x) / (shRenderer.bounds.size.x) * Mathf.Pow(this.transform.localScale.x, -1) + (float)0.001;
+        skinHolder.transform.localScale = new Vector3(adjustedScale, adjustedScale, 1);
+
+
+        
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
