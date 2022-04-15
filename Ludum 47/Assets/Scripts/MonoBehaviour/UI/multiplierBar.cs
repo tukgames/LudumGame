163bd9f7c16e1c648538b88a66d5438cf61763e8
TacoTalon <88513496+TacoTalon@users.noreply.github.com>
2022-04-15 19:57:38 -0400

    Multiplier bar counts down time

--- Ludum 47/Assets/Scripts/MonoBehaviour/UI/multiplierBar.cs
+++ Ludum 47/Assets/Scripts/MonoBehaviour/UI/multiplierBar.cs
@@ -18,29 +18,28 @@ public class multiplierBar : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        float width = Mathf.Lerp(bar.sizeDelta.x, targ, Time.deltaTime*8);
+    void Update() 
+     {
+        Debug.Log(sManager.timerLose);
+        if (sManager.multiplier != 1) {
+            updateMultBarWidth(sManager.timerLose);
+        } else {
+            updateMultBarWidth((float)0);
+        }
+
+        float width = Mathf.Lerp(bar.sizeDelta.x, targ, Time.deltaTime * 8);
         bar.sizeDelta = new Vector2(width, (float)7.5);
     }
 
-    public void updateMultBarWidth(float multiplier) {
-        multiplier--;
-        float scale = (mainCanvas.GetComponent<RectTransform>().sizeDelta.x) / 2 / 6;
-
-        // rational function
-        // float targ = scale * (-(9) / (multiplier + 2) + 4.5);
-        float val;
-        // square root
-        if(multiplier < 9) {
-            val = 2 * Mathf.Pow(multiplier, (float)0.5);
-        } else {
-            val = 6;
+    public void updateMultBarWidth(float val) {
+            if (val == 0) {
+                targ = 0;
+            } else {
+                targ = -(1 / sManager.timeToLoseMult) * val + 1;
+                targ *= 600;
+            }
+            if (targ < 0) {
+                targ = 0;
+            }
         }
-
-        targ = val * scale;
-        
-        
     }
-
-}
